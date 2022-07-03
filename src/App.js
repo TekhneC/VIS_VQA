e52015b94feed5ed91124af93ae0cccffd2d0c0f
unknown <rachelchen030223@outlook.com>
2022-07-03 22:04:16 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 import {Layout} from 'antd';
+import { Divider } from 'antd';
 import React from 'react';
+import './Control.js'
+
+
 
 const { Header, Footer, Sider, Content } = Layout;
 const App = () => (
   <>
     <Layout>
-      <Sider>Sider</Sider>
+      <Sider>
+        <Divider>控制台</Divider>
+        <div id = "control"></div>
+        <Divider>辅助面板</Divider>
+        <div id = "Assist"></div>
+      </Sider>
       <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
+        <Header><span>VQA数据可视化分析</span></Header>
+        <Content><div id = "Overview"></div>Content</Content>
+        <Footer><div id = "Details"></div></Footer>
       </Layout>
     </Layout>
   </>
 );
+
+
 
 export default App;
 
---
+++ src/Control.js
@@ -0,0 +1,16 @@
+import { Button } from 'antd';
+import { AutoComplete, Input } from 'antd';
+import React, { useState } from 'react';
+
+
+class Control extends React.Component{
+    render(){
+        return(
+            <div>
+                <></>
+                <Button type="primary">问题视角</Button>
+                <Button type="primary">答案视角</Button>    
+            </div>
+        );
+    }
+}
