b69a7780adbd16f575e5c80adfdfee22c7d5fe8f
unknown <rachelchen030223@outlook.com>
2022-07-03 20:30:01 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {Layout} from 'antd';
+import React from 'react';
+
+const { Header, Footer, Sider, Content } = Layout;
+const App = () => (
+  <>
+    <Layout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+  </>
+);
+
+export default App;
+
+<style>
+</style>
