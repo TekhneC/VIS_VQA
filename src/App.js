e752c719d6d17ed3b7befbd4395f3762d08ecf10
unknown <rachelchen030223@outlook.com>
2022-07-06 09:27:54 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,104 @@
 import './App.css';
-import {Layout} from 'antd';
-import { Divider } from 'antd';
+import {Layout,Divider,Button,Input,Space} from 'antd';
 import React from 'react';
-import {Control} from './Control';
-import { Assist } from './Assist';
+import { AudioOutlined } from '@ant-design/icons';
+import { Assist } from './Block/Assist';
+import { AnsPes,QuesPes,AnsComp,QuesComp } from './Block/Overview';
+import{ Detail } from './Block/Detail';
 import { WordsClouds } from './graphs/WordsCloud';
 import { StackedBar } from './graphs/StackedBar';
+import * as echarts from 'echarts/core';
+import {$,jQuery} from 'jquery';
+import data from "./Data/Abstract.json";
 
 const { Header, Footer, Sider, Content } = Layout;
-const App = () => (
-  <>
-    <Layout>
-      <Sider  collapsedWidth = "300" width="300">
-        <Divider>控制台</Divider>
-        <div id = "control">
-        <Control></Control>
-        </div>
-        <Divider>辅助面板</Divider>
-        <div id = "Assist">
-        <Assist Rdivid = "Radar" Wdivid = "WordsClouds"></Assist>
-        </div>
-      </Sider>
-      <Layout>
-        <Header><span>VQA数据可视化分析</span></Header>
-        <Content id = "Overview"><div id = "Overviews"></div>Content</Content>
-        <Content id = "Detail">
-        <StackedBar id = "hrer"></StackedBar></Content>
-        <Footer ><div id = "footer"></div></Footer>
-      </Layout>
-    </Layout>
-  </>
-);
+const { Search } = Input;
+const onSearch = (value) => console.log(value);
 
 
+export class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      dataGet: 0,
+      totalData:data,
+      overviewRender:'',
+      isQuesPes:1
+    }
+  }
+/*
+  getData(){
+    fetch('./Abstract.json', {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        },
+    })
+    .then(response => response.json())//解析为Promise
+    .then(data => {
+    
+    this.setState({dataGet:1,totalData: data})  ////赋值到本地数据
+    console.log(this.state.totalData)
+   
+    })
+  }
+*/
 
-export default App;
+  ShowComponent(block){
+      this.setState({
+        overviewRender:block
+      })
+  }  
+
+  PesSet = (i) => {
+    this.setState({
+      isQuesPes: i
+    })
+  }
+
+  renderComponent = ()=>{
+    switch(this.state.overviewRender){
+      case 'QuesPes': return <QuesPes/>
+      case 'AnsPes' :return <AnsPes/>
+      case 'QuesComp' : return <QuesComp/>
+      case 'AnsComp' : return <AnsComp/>
+      default : return <QuesPes/>
+    }
+  }
+
+  render(){
+    //if(this.state.dataGet === 0) this.getData(); 
+    return (
+      <div id = "Apps">
+        <Layout>
+          <Sider  collapsedWidth = "300" width="300">
+            <Divider>控制台</Divider>
+            <div id = "control">
+              <Search id = "search" placeholder="输入以搜索数据~" onSearch={onSearch} enterButton />
+              <Button id = "Ques" block = "True" onClick={() => {this.PesSet(1);this.ShowComponent('QuesPes')}}>问题视角</Button>
+              <Button id = "Ans" block = "True" onClick={() => {this.PesSet(0);this.ShowComponent('AnsPes')}}>答案视角</Button>
+              <Button id = "Comp" block = "True" onClick={() => {this.state.isQuesPes ? this.ShowComponent('QuesComp'):this.ShowComponent('AnsComp')}}>对比数据</Button>
+            </div>
+            <Divider>辅助面板</Divider>
+            <div id = "Assist">
+            <Assist Data = {this.state.totalData}></Assist>
+            </div>
+          </Sider>
+          <Layout>
+            <Header><span>VQA数据可视化分析</span></Header>
+            <Content id = "Overview">
+              {this.renderComponent()}
+            </Content>
+            <Content id = "Detail">
+              <Detail Data = {this.state.totalData}></Detail></Content>
+            <Footer ><div id = "footer"></div></Footer>
+          </Layout>
+        </Layout>
+      </div>
+    );
+    }
+}
 
 <style>
 </style>
